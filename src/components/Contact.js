f83649a09d76a82d30be897083ed869950f1707c
awoods1893 <awoods1983@gmail.com>
2022-10-31 11:34:49 -0500

    continuing to style and format the contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,68 @@
-import React from "react";
+import React, {useRef} from "react";
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-  return (
-    <div className="contact">
+const form= useRef();
+const serviceID = "service_ID"
+const templateID = "template_ID"
+const publicKey = "nIRBKR2aFrgmboRlG"
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm(serviceID, templateID, form.current, publicKey)
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+            });
+        };
+        
+        return (
+    <div className="contact bg-secondary">
         <div className="text-center">
         <h1>Contact Me</h1>
-        <p>I can be reached at my email address: awoods1983@gmail.com or on my social media.</p>
+        <p>Please fill out the form below. I'd love to hear from you!</p>
+        </div>
+        <div className="container">
+      <form onSubmit={sendEmail}>
+
+            <div className="row">
+                <div className="col-md-6 col-xs-12">
+                    <input
+                    id="name"
+                    type="text" 
+                    className="form-control"
+                    placeholder="Name"
+                    name="name"
+                    />
+                    <input 
+                    id="email"
+                    type="email"
+                    className="form-control"
+                    placeholder="Email address"
+                    name="email"
+                    />
+                <input 
+                    id="subject"
+                    type="text"
+                    className="form-control"
+                    placeholder="Subject"
+                    name="subject"
+                    />
+
+                </div>
+                <div className="col-md-6 col-xs-12">
+                    <textarea
+                    type="text"
+                    className="form-control"
+                    placeholder="Message"
+                    name="message"
+                    ></textarea>
+                    <button className="btn bg-light" type="submit">Contact Me</button>
+                </div>
+            </div>
+                    </form>
         </div>
         </div>
        
