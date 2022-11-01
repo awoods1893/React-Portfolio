511a6a66129b306f657dec325ed72dbd8e9726fe
awoods1893 <awoods1983@gmail.com>
2022-11-01 09:19:05 -0500

    finised the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
 
 const Footer = () => {
   return (
@@ -14,6 +16,33 @@ const Footer = () => {
                     </div>
                     <div className="d-flex">
                         <p>awoods@gmail.com</p>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-2 col-sm-6">
+                    <div className="row">
+                        <div className="col">
+                            <a className="footer-nav">Home</a>
+                            <br/>
+                            <a className="footer-nav">About Me</a>
+                            <br/>
+                            <a className="footer-nav">Portfolio</a>
+                        </div>
+                        <div className="col">
+                            <a className="footer-nav">Contact</a>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="col-lg-5 col-md-5 col-sm-6 align-items-center">
+                    <div className="d-flex justify-content-center">
+                        <a href="https://www.linkedin.com/in/aaron-woods-7981205/">
+                    <FontAwesomeIcon icon="fa-brands fa-github" />
+                    </a>
+                    <div className="d-flex justify-content-center">
+                        <a href="https://github.com/awoods1893">
+                    <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+                    </a>
+                        </div>
                     </div>
                 </div>
             </div>
